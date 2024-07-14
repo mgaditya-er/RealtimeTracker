a3b86b4834005a623629f0a1382d06b5516d09ad
mgadity-er <aditya_ghyar@moderncoe.edu.in>
2024-07-14 14:04:32 +0530

    file: css and js for map feel

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const app = express();
+
+const http = require('http');
+
+const sockerio = require('socket.io');
+const server = http.createServer(app);
+const io = sockerio(server);
+
+app.set('view engine', 'ejs');
+app.set(express.static(__dirname + 'public'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
